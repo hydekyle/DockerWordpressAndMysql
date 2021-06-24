4ddd6e44a80921e2c090ca45f00f324e864cc882
hydekyle <hydekyle@hotmail.com>
2021-06-24 19:22:32 +0100

    wp

--- html/update-stats.php
+++ html/update-stats.php
@@ -4,25 +4,24 @@
     include_once("./wp-includes/wp-db.php");
 
     $player_id = $_GET["id"];
-    $player_rating = $_GET["player_rating"]
-    $player_type = $_GET["player_type"]
-    $player_goals = $_GET["player_goals"]
-    $player_minutes = $_GET["player_minutes"]
-    $player_fouls_committed = $_GET["player_fouls_committed"]
-    $player_duels_total = $_GET["player_duels_total"]
-    $player_duels_won = $_GET["player_duels_won"]
-    
+    $player_rating = $_GET["rating"];
+    $player_type = $_GET["type"];
+    $player_goals = $_GET["goals"];
+    $player_minutes = $_GET["minutes"];
+    $player_fouls_commited = $_GET["fouls_commited"];
+    $player_duels_total = $_GET["duels_total"];
+    $player_duels_won = $_GET["duels_won"];
     global $wpdb;
     $wpdb->select("Magnificos");
     $result = $wpdb->insert('Futbolistas_Stats', array(
         'idFutbolista' => $player_id,
-        'player_rating' => 9.20,
-        'player_type' => "2222",
-        'player_goals' => 2,
-        'player_minutes' => 2,
-        'player_fouls_committed' => 2,
-        'player_duels_total' => 2,
-        'player_duels_won' => 2
+        'player_rating' => $player_rating,
+        'player_type' => $player_type,
+        'player_goals' => $player_goals,
+        'player_minutes' => $player_minutes,
+        'player_fouls_committed' => $player_fouls_commited,
+        'player_duels_total' => $player_duels_total,
+        'player_duels_won' => $player_duels_won
     )) or die (mysql_error());
     echo ($result);
 ?>
